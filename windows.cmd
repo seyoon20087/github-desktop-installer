d51f4587124340648ddd6b92dd1caf5a41184258
Shinwon Elizabeth Yoon <24852454+seyoon20087@users.noreply.github.com>
2021-03-26 13:47:38 +0900

    Update windows.cmd

--- windows.cmd
+++ windows.cmd
@@ -4,7 +4,6 @@ if %OS%==32BIT goto abort32bit
 
 
 echo "Downloading file"
-powershell $client.DownloadFile("https://central.github.com/deployments/desktop/desktop/latest/win32","C:\tmp\GitHubDesktopSetup.exe")
 powershell (new-object System.Net.WebClient).DownloadFile('https://central.github.com/deployments/desktop/desktop/latest/win32','%Downloads%\GitHubDesktopSetup.exe')
 echo "Opening GitHub Desktop Installer"
 C:\tmp\GitHubDesktopSetup.exe
