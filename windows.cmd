fc367741961359b27333258c5aa9b1d5eb89148f
Shinwon Elizabeth Yoon <24852454+seyoon20087@users.noreply.github.com>
2021-03-26 12:54:06 +0900

    Update windows.cmd

--- windows.cmd
+++ windows.cmd
@@ -1,3 +1,6 @@
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
+
+if %OS%==32BIT goto abort32bit
 echo "Downloading file"
 powershell $client.DownloadFile("https://central.github.com/deployments/desktop/desktop/latest/win32","C:\tmp\GitHubDesktopSetup.exe")
 powershell (new-object System.Net.WebClient).DownloadFile('https://central.github.com/deployments/desktop/desktop/latest/win32','%Downloads%\GitHubDesktopSetup.exe')
@@ -5,3 +8,6 @@ echo "Opening GitHub Desktop Installer"
 C:\tmp\GitHubDesktopSetup.exe
 erase C:\tmp\GitHubDesktopSetup.exe
 exit /b 0
+:abort32bit
+echo "You must use a 64-bit operating system to run GitHub Desktop."
+exit /b 1
