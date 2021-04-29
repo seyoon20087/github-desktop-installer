94d2d00bc346cf8121e6cbc4c46db38feac3741b
seyoon20087 <24852454+seyoon20087@users.noreply.github.com>
2021-04-29 20:41:01 +0900

    Update windows.cmd

--- windows.cmd
+++ windows.cmd
@@ -5,14 +5,14 @@ if %OS%==32BIT goto abort32bit
 if %OS%==64BIT goto startinstall
 
 :startinstall
-echo "Downloading file"
-curl --request GET "https://central.github.com/deployments/desktop/desktop/latest/win32" --ouput %Downloads%\GitHubDesktopSetup.exe -LO
-echo "Opening GitHub Desktop Installer"
-%Downloads%\GitHubDesktopSetup.exe
+echo Downloading file
+curl --request GET "https://central.github.com/deployments/desktop/desktop/latest/win32" --output .\GitHubDesktopSetup.exe -LO
+echo Opening GitHub Desktop Installer
+.\GitHubDesktopSetup.exe
 exit /b 0
 
 :abort32bit
-echo "You must have a 64-bit operating system to run GitHub Desktop."
-echo "Please see the following documentation for more info:"
-echo "https://docs.github.com/en/desktop/installing-and-configuring-github-desktop/supported-operating-systems#about-supported-operating-systems"
+echo You must have a 64-bit operating system to run GitHub Desktop.
+echo Please see the following documentation for more info:
+echo https://docs.github.com/en/desktop/installing-and-configuring-github-desktop/supported-operating-systems#about-supported-operating-systems
 exit /b 1
